58b8e5eb6c3f0c609ad2e0d95b5c2fdc0a053858
william_office_pc <wangchenglong@kingmedpharm.com>
2025-06-24 10:08:31 +0800

    删除之前的防篡改检验

--- ISAT/annotation.py
+++ ISAT/annotation.py
@@ -61,13 +61,13 @@ class Annotation:
                     try:
                         dataset = loads(data)
                         file_format = 'json'
-                    except Exception as e:
-                        print('read json error')
-                        print(e)
-                result, save_mark, computed_mark = self.check_mark(dataset, self.SALT, self.KEY)
-                if not result:
-                    self.json_state = '标注文件存在问题，请联系管理员'
-                    return self
+                    except :
+                        self.json_state = '标注文件存在问题，请联系管理员'
+                        return self
+                # result, save_mark, computed_mark = self.check_mark(dataset, self.SALT, self.KEY)
+                # if not result:
+                #     self.json_state = '标注文件存在问题，请联系管理员'
+                #     return self
                 info = dataset.get('info', {})
                 description = info.get('description', '')
                 if description == 'ISAT':
@@ -133,7 +133,8 @@ class Annotation:
             object['note'] = obj.note
             dataset['objects'].append(object)
         with open(self.label_path, 'wb') as f:
-            a = dumps(self.add_mark(dataset, self.SALT, self.KEY), indent=4)
+            # a = dumps(self.add_mark(dataset, self.SALT, self.KEY), indent=4)
+            a = dumps(dataset, indent=4)
             magic, version, data_len, output_len, output = encry(a.encode("utf-8"), '', '')
             f.write(magic)
             f.write(version.to_bytes(4, byteorder='little'))
@@ -143,53 +144,53 @@ class Annotation:
 
         return True
 
-    @staticmethod
-    def get_mark(js_data: dict, salt: str) -> str:
-        js_str = json.dumps(js_data)
-        md5 = hashlib.md5()
-        md5.update(salt.encode("utf-8"))
-        md5.update(js_str.encode("utf-8"))
-        return md5.hexdigest()
-
-    def add_mark(self, js_data: dict, salt: str, key: str) -> dict:
-        '''
-        info:
-            给 js_data 这个 json 对象计算出在不包含摘要时的摘要值
-        params:
-            js_data[dict]: json 对象，可以包含 mark 摘要，也可以不包含
-            salt[str]: 生成摘要时的盐
-            key[str]: 摘要生成后存放在 js_data 中的 key， 只支持1级目录
-
-        return [dict]: 包含摘要值的 json 对象
-        '''
-        result = js_data.copy()
-        if key in result:
-            del result[key]  # 删掉之前的mark
-
-        result[key] = self.get_mark(result, salt)
-        return result
-
-    def check_mark(self, js_data: dict, salt: str, key: str):
-        '''
-        info:
-            检查 js_data 这个 json 对象中存储的摘要值和不包含摘要时的摘要值是否一致
-        params:
-            js_data[dict]: json 对象，不包含摘要值则当成""处理
-            salt[str]: 计算摘要时的盐
-            key[str]: 摘要存放在 js_data 中的 key， 只支持1级目录
-
-        return:
-            result[bool]: 是否一致
-            save_mark[str]: 存在js_data中的摘要值
-            computed_mark[str]: 实际计算出来的摘要值
-        '''
-        save_mark = js_data.get(key, "")
-        if save_mark != "":
-            js_data_remove_mark = js_data.copy()
-            del js_data_remove_mark[key]
-        else:
-            js_data_remove_mark = js_data
-
-        computed_mark = self.get_mark(js_data_remove_mark, salt)
-
-        return save_mark == computed_mark, save_mark, computed_mark
+    # @staticmethod
+    # def get_mark(js_data: dict, salt: str) -> str:
+    #     js_str = json.dumps(js_data)
+    #     md5 = hashlib.md5()
+    #     md5.update(salt.encode("utf-8"))
+    #     md5.update(js_str.encode("utf-8"))
+    #     return md5.hexdigest()
+    #
+    # def add_mark(self, js_data: dict, salt: str, key: str) -> dict:
+    #     '''
+    #     info:
+    #         给 js_data 这个 json 对象计算出在不包含摘要时的摘要值
+    #     params:
+    #         js_data[dict]: json 对象，可以包含 mark 摘要，也可以不包含
+    #         salt[str]: 生成摘要时的盐
+    #         key[str]: 摘要生成后存放在 js_data 中的 key， 只支持1级目录
+    #
+    #     return [dict]: 包含摘要值的 json 对象
+    #     '''
+    #     result = js_data.copy()
+    #     if key in result:
+    #         del result[key]  # 删掉之前的mark
+    #
+    #     result[key] = self.get_mark(result, salt)
+    #     return result
+    #
+    # def check_mark(self, js_data: dict, salt: str, key: str):
+    #     '''
+    #     info:
+    #         检查 js_data 这个 json 对象中存储的摘要值和不包含摘要时的摘要值是否一致
+    #     params:
+    #         js_data[dict]: json 对象，不包含摘要值则当成""处理
+    #         salt[str]: 计算摘要时的盐
+    #         key[str]: 摘要存放在 js_data 中的 key， 只支持1级目录
+    #
+    #     return:
+    #         result[bool]: 是否一致
+    #         save_mark[str]: 存在js_data中的摘要值
+    #         computed_mark[str]: 实际计算出来的摘要值
+    #     '''
+    #     save_mark = js_data.get(key, "")
+    #     if save_mark != "":
+    #         js_data_remove_mark = js_data.copy()
+    #         del js_data_remove_mark[key]
+    #     else:
+    #         js_data_remove_mark = js_data
+    #
+    #     computed_mark = self.get_mark(js_data_remove_mark, salt)
+    #
+    #     return save_mark == computed_mark, save_mark, computed_mark
